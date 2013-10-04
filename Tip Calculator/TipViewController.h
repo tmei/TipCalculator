6e732c7ec2925400f7491a43315629aff39e8ab4
Tatung Mei <tmei@zynga.com>
2013-10-04 15:37:24 -0700

    Tip Calculator

---
+++ Tip Calculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  Tip Calculator
+//
+//  Created by Z_ZyngaTesting on 10/2/13.
+//  Copyright (c) 2013 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
---
+++ Tip Calculator/TipViewController.m
@@ -0,0 +1,63 @@
+//
+//  TipViewController.m
+//  Tip Calculator
+//
+//  Created by Z_ZyngaTesting on 10/2/13.
+//  Copyright (c) 2013 codepath. All rights reserved.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
+- (IBAction)onTap:(id)sender;
+- (void)updateValues;
+
+@end
+
+@implementation TipViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        self.title = @"Cheap Tip Calculator";
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    [self updateValues];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+    [self updateValues];
+}
+
+- (void)updateValues {
+    float billAmount = [self.billTextField.text floatValue];
+    NSArray *tipValues = @[@(0.01), @(0.03), @(0.05)];
+    float tipAmount = billAmount * [tipValues[self.tipControl.selectedSegmentIndex] floatValue];
+    float totalAmount = tipAmount + billAmount;
+    self.tipLabel.text = [NSString stringWithFormat:@"$%0.2f", tipAmount];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%0.2f", totalAmount];
+    
+                           
+}
+
+@end
